e52911212356850bd403c1ea7e9138c8e08d77eb
Daria Wu <dwu891@gmail.com>
2017-05-07 16:20:22 -0700

    Added vue project files

--- test.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-  document.getElementById('preset1').onclick = function() {
-    document.getElementById('skill_sequence').value = 'F A3 A3 A2 A2 G G B1';
-    input_run(); };
-
-  document.getElementById('preset2').onclick = function() {
-    document.getElementById('skill_sequence').value = 'F K F A2 A2 T3 T3 B1';
-    input_run(); };
-};
